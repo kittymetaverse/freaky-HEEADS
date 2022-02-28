2b6a93ec56ca31a21953567c9ec9e9b841189535
kittymetaverse <99939005+kittymetaverse@users.noreply.github.com>
2022-02-28 16:03:21 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -201,8 +201,6 @@ function App() {
         style={{ padding: 24, backgroundColor: "var(--primary)" }}
         image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : null}
       >
-        <a href={CONFIG.MARKETPLACE_LINK}>
-          <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
         </a>
         <s.SpacerSmall />
         <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
